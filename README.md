d42f092b174c361c8f2abf57ffc45c33ee11ff49
Jan Piotraschke <janpiotraschke@horizon.speedport.ip>
2022-06-07 11:39:42 +0200

    added requirements file; now able to convert the notebook to python code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Fachkurs_Systembiology
 
 Das Repository für den Fachkurs "Systems Biology" im SS2022
+Converts your Jupyter Notebook to python script: `jupyter nbconvert --to script *.ipynb`
--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-# !pip3 install scipy pandas numpy matplotlib
+#!/usr/bin/env python
+# coding: utf-8
 
+# <a href="https://colab.research.google.com/github/Jan-Piotraschke/fachkurs_systembiology/blob/main/main.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+
+# In[3]:
+
+
+# !pip3 install scipy pandas numpy matplotlib
+#!pip3 install voila  # Voilà works with any Jupyter kernel (C++, Python, Julia)
 from scipy.integrate import odeint
 import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
 # %matplotlib inline
+
+# import ipywidgets as widgets  # make this notebook interactive
+
+
+# In[23]:
+
 
 # parameters
 k_RL = 2 * 10 ** 6
@@ -17,10 +31,13 @@ k_G1 = 1
 k_Ga = 10 ** -5
 
 Gt = 10 ** 4  # total number of G proteins per cell
-L =  10 ** -6  # alpha-factor -> 1 uM
 
 
-def solve_ode_system(current_state, t, parameter) -> list:
+
+# In[27]:
+
+
+def solve_ode_system(current_state, t, k_Gd, L) -> list:
     """
 
     :param current_state: current state of the ODE system
@@ -29,9 +46,6 @@ def solve_ode_system(current_state, t, parameter) -> list:
     :return: solution of the ODE System
     """
     R, RL, G, Ga = current_state
-
-    # variable parameter
-    k_Gd = parameter
 
     # algebraic equations:
     Gd = Gt - G - Ga  # Galpha-GDP
@@ -46,6 +60,10 @@ def solve_ode_system(current_state, t, parameter) -> list:
     return [dR_dt, dRL_dt, dG_dt, dGa_dt]
 
 
+
+# In[52]:
+
+
 time = np.arange(0, 600, 0.01)
 
 # initial values
@@ -58,29 +76,33 @@ S0 = [R_0, RL_0, G_0, Ga_0]
 
 # variable rate constant for G protein deactivation
 k_Gd = 0.004   # 0.11  # 0.004
-solution = odeint(solve_ode_system, S0, time, args=(k_Gd,))
+L_alpha =  10 ** -6  # alpha-factor -> 1 uM
+solution = pd.DataFrame(odeint(solve_ode_system, S0, time, args=(k_Gd, L_alpha)), columns=['R', 'RL', 'G', 'Ga'])
 
-fig = plt.figure(1, figsize=(8, 4))
+dose_response = {}
+for L_var in [0.03, 0.1, 0.2, 0.6, 1, 10, 100, 1000]:  # nM alpha-factor 
+    data = pd.DataFrame(odeint(solve_ode_system, S0, time, args=(k_Gd, L_var*10**-3)), columns=['R', 'RL', 'G', 'Ga'])
+    dose_response[L_var] = data
+
+
+# In[47]:
+
+
+fig, ax = plt.subplots(figsize=(8, 4)) #, ncols=2)
 
 # choose the style of the plot
 # print(plt.style.available)
 # plt.style.use('classic')
-
-ax = plt.axes()
-ax.set_xlabel("Time sec")
-ax.set_ylabel("Counts per cell")
+ax.set_xlabel("Time [secs]")
+ax.set_ylabel("Active G-Protein (Fraction)")
 
 # Hide the right and top spines
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
-
+ax.plot(time, solution["Ga"]/Gt)  # Fig. 5A from the paper 
 ax.legend()
 
-plt.plot(time, solution)
-
-# ax.axhline(y=ICU_capacity, color='gray',alpha = 0.4, label='ICU Capacity')
 # plt.legend(loc=(1.1,0.5), frameon=False)
-plt.legend(frameon=False)
 
 # ax.xaxis.set_ticks([0,60,120])
 # ax.set_xlim(0,200)
@@ -88,5 +110,13 @@ plt.legend(frameon=False)
 ax.yaxis.grid(alpha=0.4)
 
 # plt.savefig('GA_Deaths_40days.png', dpi=600, bbox_inches='tight')
+fig1, ax1 = plt.subplots(figsize=(8, 4)) #, ncols=2)
+ax1.set_xlabel('log[Alpha-Factor] [nM]')
+ax1.set_ylabel('G-Protein Activation Response')
 
 plt.show()
+
+# In[ ]:
+
+
+
